73d7bd57528e8c76d2493e70805f59beb56dcff4
Arslan Mughal <iammarslanmughal@gmail.com>
2024-03-13 15:01:40 +0500

    Variable Let Const

--- variable.js
+++ variable.js
@@ -1,18 +1,42 @@
 /*  Data Types
 undefined, null, boolean, string, symbol, number, and object  */
 
+// --------- Var Start----------------
+
 var myName = "ABCD"
 myName = 81
 
-console.log(myName)
+var first, second;
+first= 45;
+
+// dobara sa variable ko declare kr sakhty hai
+
+// document.write(myName)  
+// document.write(first)
 
 
+// --------- Var End----------------
+// --------- Let Start----------------
+
+let LastName = "ASDF"
+LastName= "QWER"
+// Let LastName="asdf"   Dobara Declare ni kr sakhty
+
+// dobara sa variable ko declare (Nhi) kr sakhty hai 
+// value overwrite hu sakhti hai
+
+// document.write(LastName)
+
+// --------- Let End----------------
+// --------- Const Start----------------
+
+const Second = "Hello"
+Second = "World"
+
+// const ma dobara value ko declare or define ni kr sakhty
+
+// document.write(Second)
 
 
-
-
-
-
-
-
+// --------- Const End----------------
 
