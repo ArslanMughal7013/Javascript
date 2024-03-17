09f75f8c99a8fe49948dbcf4a6048cbb63e52d4f
Arslan Mughal <iammarslanmughal@gmail.com>
2024-03-17 23:57:36 +0500

    If else condition with greater than

--- variable.js
+++ variable.js
@@ -210,6 +210,20 @@ function testEqual(val){
 }
 console.log(testEqual(10))
 // -----------------------------
+// check conditon greater than if else
+function greaterThenValue(val){
+    if(val>100){
+        return "Over 100"
+    }
+    if(val>10){
+        return "Over 10"
+    }
+    return "10 or under"
+}
+
+console.log(greaterThenValue(10))
+
+// ------------------------------
 
 
 
